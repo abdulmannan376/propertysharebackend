601a05483b70bbf24b5d576a605e423ed0c1c094
faizandeveloper11 <faizan.fs.1998@gmail.com>
2025-05-09 15:58:27 +0500

    comment the original send email and added new for testing

--- helpers/emailController.js
+++ helpers/emailController.js
@@ -1,31 +1,74 @@
-const sgMail = require('@sendgrid/mail');
+//////////////////uncomment this/////////////////////////////////////////////////
+// const sgMail = require('@sendgrid/mail');
 
-// Set your SendGrid API Key
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// // Set your SendGrid API Key
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Email sending function
+// // Email sending function
+// function sendEmail(recipient, subject, body) {
+//   const msg = {
+//     to: recipient, // Recipient email
+//     from: 'info@beachbunnyhouse.com', // Verified SendGrid sender email
+//     subject: subject,
+//     text: body, // HTML content for the email
+//   };
+
+//   sgMail
+//     .send(msg)
+//     .then(() => {
+//       // Email sent successfully
+//       console.log(`Email with subject "${subject}" sent to ${recipient}`);
+//     })
+//     .catch((error) => {
+//       // Log error details
+//       console.error('Error sending email:', {
+//         message: error.message,
+//         response: error.response ? error.response.body : null,
+//         timestamp: new Date().toLocaleString(),
+//       });
+//     });
+// }
+// module.exports = { sendEmail };
+/////////////////////unComment this///////////////////////////////////////////////
+
+/////////////////////Comment this///////////////////////////////////////////////
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "balaj.ali707@gmail.com",
+    pass: "zyyo rgfk dsrr wxfx",
+  },
+});
+
+const currentDateMilliseconds = Date.now();
+const currentDateString = new Date(currentDateMilliseconds).toLocaleString();
+
 function sendEmail(recipient, subject, body) {
-  const msg = {
-    to: recipient, // Recipient email
-    from: 'info@beachbunnyhouse.com', // Verified SendGrid sender email
+  const mailOptions = {
+    from: "balaj.ali707@gmail.com",
+    to: recipient,
     subject: subject,
-    text: body, // HTML content for the email
+    text: body,
   };
-
-  sgMail
-    .send(msg)
-    .then(() => {
-      // Email sent successfully
-      console.log(`Email with subject "${subject}" sent to ${recipient}`);
-    })
-    .catch((error) => {
-      // Log error details
-      console.error('Error sending email:', {
-        message: error.message,
-        response: error.response ? error.response.body : null,
-        timestamp: new Date().toLocaleString(),
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(`Error: ${error}`, "location: ", {
+        function: "sendMail",
+        fileLocation: "controllers/PropertyController.js",
+        timestamp: currentDateString,
       });
-    });
+    } else {
+      // console.log(
+      //   `success in function: sendEmail, file location: controllers/UsersController.js, timestamp: ${currentDateString}`
+      // );
+      console.log(`Mail with Subject: ${subject}, sent to ${recipient}`);
+    }
+  });
 }
 
 module.exports = { sendEmail };
+
+/////////////////////Comment this///////////////////////////////////////////////
